4b8c3bf84f12e3bf33f515be433e40a1473b49a5
yali <yusufali0147@hotmail.com>
2023-08-21 00:59:30 +0300

    modified:   src/Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Artık Yıl Hesaplama Programı");
+        int yil;
+        Scanner imp = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        yil = imp.nextInt();
+        if (((yil%4==0)&&((yil%100)!=0))||(yil%400==0)){
+            System.out.println(yil+" bir artık yıldır.");
+        }else{
+            System.out.println(yil+" bir artık yıl değildir.");
+        }
     }
 }
